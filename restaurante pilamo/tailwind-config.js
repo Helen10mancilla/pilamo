5783589c60c23143aa7ed82210336368584e6e8b
Helen orejuela mancilla <135240673+Helen10mancilla@users.noreply.github.com>
2026-02-13 15:02:56 -0500

    new proyect

---
+++ restaurante pilamo/tailwind-config.js
@@ -0,0 +1,23 @@
+tailwind.config = {
+    darkMode: "class",
+    theme: {
+        extend: {
+            colors: {
+                "primary": "#17cf36",
+                "background-light": "#f6f8f6",
+                "background-dark": "#0a0f0b",
+                "surface-dark": "#112114",
+            },
+            fontFamily: {
+                "display": ["Be Vietnam Pro", "sans-serif"],
+                "serif": ["Playfair Display", "serif"]
+            },
+            borderRadius: {
+                "DEFAULT": "0.25rem",
+                "lg": "0.5rem",
+                "xl": "0.75rem",
+                "full": "9999px"
+            },
+        },
+    },
+}
